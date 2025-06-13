e79346a5439691ec73a793dc3c3bc3a1dc5a73cc
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-13 11:39:20 +0530

    Heap Sort

--- HeapSort.c
+++ HeapSort.c
@@ -1,5 +1,22 @@
 #include<stdio.h>
-int main(){
-    
 
+int main(){
+    int n;
+    scanf("%d",&n);
+    printf("Enter Heap Size: ");
+    int a[n];
+    printf("\nEnter Numbers: ");
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("Array Before sort: ");
+    for(int i=0;i<n;i++){
+        printf("%d",a[i]);
+    }
+    heapsort(a,0);
+    printf("Array after sort: ");
+    for(int i=0;i<n;i++){
+        printf("%d",a[i]);
+    }
+    return 0;
 }
