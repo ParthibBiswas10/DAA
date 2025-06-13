301f6dbb0a19e034c227099fe251c88b9bc27bbd
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-13 12:06:45 +0530

    Heap Sort

--- HeapSort.c
+++ HeapSort.c
@@ -1,24 +1,35 @@
 #include<stdio.h>
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
 void maxheapify(int a[], int i, int size){
     int l=2*i;
     int r=(2*i)+1;
     int largest=i;
-    if(l<=size && a[l]>a[largest]) swap(a[l], a[largest]);
-    if(r<=size && a[r]>a[largest]) swap(a[r], a[largest]);
+    if(l<=size && a[l]>a[largest]) swap(&a[l], &a[largest]);
+    if(r<=size && a[r]>a[largest]) swap(&a[r], &a[largest]);
 
     if(largest!=i){
-        swap(a[i],a[largest]);
+        swap(&a[i],&a[largest]);
         maxheapify(a,largest,size);
     }
 
 
 }
-void heapsort(int a, int n){
+void heapsort(int a[], int n){
     //MaxHeap Creation Start :(
     for(int i=n/2; i>=1; i--){
         maxheapify(a,i,n);
     }
      //MaxHeap Creation Ends :)
+
+     for(int i=n;i>=2;i--){
+        swap(&a[i],&a[1]);
+        n--;
+        maxheapify(a,1,n);
+     }
 }
 int main(){
     int n;
