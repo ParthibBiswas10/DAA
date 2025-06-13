61242dcd5c00a6581a87f312c26ed1fdd44ff59b
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-13 12:24:25 +0530

    Heap Sort

--- HeapSort.c
+++ HeapSort.c
@@ -25,17 +25,19 @@ void maxheapify(int a[], int i, int size){
 }
 void heapsort(int a[], int n){
     //MaxHeap Creation Start :(
-    for(int i=n/2; i>=1; i--){
+    for(int i=n/2; i>=1; i--){    // O(n)
         maxheapify(a,i,n);
     }
      //MaxHeap Creation Ends :)
 
-     for(int i=n;i>=2;i--){
-        swap(&a[i],&a[1]);
+     for(int i=n;i>=2;i--){    //n times
+        swap(&a[i],&a[1]);         
         n--;
-        maxheapify(a,1,n);
+        maxheapify(a,1,n);    //O(logn)
      }
 }
+//total time complexity for heapsort: O(nlogn)//
+
 int main(){
     int n;
     printf("Enter Heap Size: ");
@@ -50,9 +52,11 @@ int main(){
         printf("%d ",a[i]);
     }
     heapsort(a,n);
+
     printf("Array after sort: ");
     for(int i=1;i<=n;i++){
         printf("%d ",a[i]);
     }
     return 0;
 }
+// at line 33 We stop at 2 because when only one element remains (at index 1), it's already in its correct place.
