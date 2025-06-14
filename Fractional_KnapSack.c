bcd613ce3726d2dcd833e2d7f3be9e6fe5b8c828
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-14 22:55:10 +0530

    KnapSack

--- Fractional_KnapSack.c
+++ Fractional_KnapSack.c
@@ -2,9 +2,20 @@
 int main(){
       int n;
     printf("Enter number of objects : ");
-  
     scanf("%d",&n);
+    int weight[n], profit[n];
+    float ratio[n];
+  
     for(int i=0;i<n;i++){
-
+        printf("wight of item %d: ",i+1);
+        scanf("%d",&weight[i]);
+        printf("profit of item %d: ",i+1);
+        scanf("%d",&profit[i]);
     }
+
+    for(int i=0;i<n;i++){
+        ratio[i]=profit[i]/weight[i];
+    }
+
+    
 }
