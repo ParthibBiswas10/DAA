f641974f5a1f5da1758836573ff50a63ec277e96
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-15 10:45:15 +0530

    jobseq

--- JobSequence.c
+++ JobSequence.c
@@ -11,23 +11,29 @@ int main(){
     struct Job j[n];
     for(int i=0;i<n;i++){
         printf("Enter Job id: ");
-        scanf("%s", &j[i].id);
+        scanf(" %s", j[i].id);
 
         printf("Enter Deadline: ");
-        scanf("%s", &j[i].deadline);
+        scanf("%d", &j[i].deadline);
 
         printf("Enter Profit : ");
         scanf("%d", &j[i].profit);
     }
+    printf("\n\n");
+//Sort the jobs based on profit in descending order
 
     for(int i=0;i<n-1;i++){
         for(int k=0;k<n-1-i;k++){
-            if(j[i].profit<j[i+1].profit){
-                struct Job temp = j[i];
-                j[i] = j[i+1];
-                j[i+1] = temp;
+            if(j[k].profit<j[k+1].profit){
+                struct Job temp = j[k];
+                j[k] = j[k+1];
+                j[k+1] = temp;
             }
         }
+    }
+    printf("Job id    Deadline    Profit\n");
+    for(int i=0;i<n;i++){
+        printf("%s   %d   %d\n", j[i].id, j[i].deadline, j[i].profit);
     }
     return 0;
 }
