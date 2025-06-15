aceabcc4e41b9dd6e77f4b15f91ab3139c896a9f
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-16 01:31:11 +0530

    Kruskal

--- kruskal.c
+++ kruskal.c
@@ -1,10 +1,19 @@
 #include<stdio.h>
+void kruskal(int v, int e, int graph[][3]){
+    //will do later
+
+}
 int main(){
     int v,e;
+    
     printf("Enter no of Vertices: ");
     scanf("%d",&v);
      printf("Enter no of Edges: ");
     scanf("%d",&e);
-    
-
+    int graph[e][3];
+    printf("Enter Source-Destination-Weight:\n");
+    for(int i=0;i<e;i++){
+        scanf("%d %d %d",&graph[i][0],&graph[i][1],&graph[i][2]);
+    }
+    kruskal(v, e, graph);
 }
