3a81e708aef656aab681c716dd3b07f103bd318f
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-17 00:25:03 +0530

    Prims

--- prims.c
+++ prims.c
@@ -1,15 +1,21 @@
 #include<stdio.h>
+void prims(){
+    
+}
+
 int main(){
     int v,e;
     
     printf("Enter no of Vertices: ");
     scanf("%d",&v);
-     printf("Enter no of Edges: ");
-    scanf("%d",&e);
-    int graph[e][3];
-    printf("Enter Source-Destination-Weight:\n");
-    for(int i=0;i<e;i++){
-        scanf("%d %d %d",&graph[i][0],&graph[i][1],&graph[i][2]);
+     //printf("Enter no of Edges: ");
+    //scanf("%d",&e);
+    int graph[v][v];
+    printf("Enter Adjacency Matrix:\n");
+    for(int i=0;i<v;i++){
+        for(int j=0;j<v;j++){
+        scanf("%d", graph[i][j]);
+        }
     }
     prims(v, e, graph);
 
