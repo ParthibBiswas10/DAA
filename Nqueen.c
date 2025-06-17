50f61cdf4115d7972bbdb9fdb3215d75ebddf4fa
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-17 12:07:17 +0530

    Nqueen

--- Nqueen.c
+++ Nqueen.c
@@ -1,10 +1,20 @@
 #include<stdio.h>
+int x[10];
+int safe(int k, int i){
+    for(int j=0;j<k;j++){
+        if(x[j]==i || abs(x[j]-i)==abs(j-k)) return 0;
+    }
+    return 1;
+}
 void nqueen(int k,int n){
     for(int i=1;i<=n;i++){
         if(safe(k,i)){
-            s[k]=i;
+            x[k]=i;
             if(k==n){
-                printf x;
+                for(int i=0;i<n;i++){
+                    printf("%d",x[i]);
+                }
+                printf("\n");
             }
             else nqueen(k+1,n);
         }
