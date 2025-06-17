a18b7cf48081cc612ebe0cc27211d0dd0a5eb115
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-17 17:26:10 +0530

    FloydWarshall

--- FloydWarshall.c
+++ FloydWarshall.c
@@ -4,10 +4,24 @@ int v=4; //for this graph;
 int graph[4][4];
 int parent[4][4];
 int distance[4][4];
+void initialize(int graph[][v]){
+    for(int i=0;i<v;i++){
+        for(int j=0;j<v;j++){
+            distance[i][j]=graph[i][j];
+        }
+    }
+}
 void floyd(int graph[][4]){
     initialize(graph);
     for(int k=0;k<v;k++){
-        
+        for(int i=0;i<v;i++){
+            for(int j=0;j<v;j++){
+                if(graph[i][j]>graph[i][k]+graph[k][j]){
+                   graph[i][j]=graph[i][k]+graph[k][j];
+                   parent[i][j]=parent[k][j];
+                }
+            }
+        }
     }
 }
 int main(){
