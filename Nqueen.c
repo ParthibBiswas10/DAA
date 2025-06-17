2740d133b08cc98ee9375803107bbc90612f4bec
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-17 11:49:12 +0530

    Nqueen

--- Nqueen.c
+++ Nqueen.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 void nqueen(int k,int n){
-    
+    for(int i=1;i<=n;i++){
+        if(safe(k,i)){
+            s[k]=i;
+            if(k==n){
+                printf x;
+            }
+            else nqueen(k+1,n);
+        }
+    }
 }
 int main(){
     int n;
