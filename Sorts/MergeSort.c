932d781fb828595a3f9bddfade2e7dfb9fd924a2
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-18 11:46:55 +0530

    MergeSort

--- Sorts/MergeSort.c
+++ Sorts/MergeSort.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 void merge(int arr[],int start , int mid, int end){
     int i=0,j=0,k=0;
-    int sortedArr[end];
+    int sortedArr[end+1];
     int n1=mid-start+1;
     int n2=end-mid;
     int left[n1];
     int right[n2];
-    for(int i=0;i<n1;i++) left[i]=arr[i];
+    for(int i=0;i<n1;i++) left[i]=arr[start+i];
     for(int i=0;i<n2;i++) right[i]=arr[mid+1+i];
 
     while(i<n1 && j<n2){
@@ -21,6 +21,24 @@ void merge(int arr[],int start , int mid, int end){
         k++;
 
     }
+    //if(i>n1){
+        while(j<n2){
+            sortedArr[k]=right[j];
+            j++;
+            k++;
+        }
+    //}
+    //else if(j>n2){
+        while(i<n1){
+            sortedArr[k]=left[i];
+            i++;
+            k++;
+        }
+    //}
+    for(int i=start,m=0;i<=end;i++,m++){
+        arr[i]=sortedArr[m];
+    }
+    
 }
 void mergesort(int arr[],int i, int j){
     if(i<j){
@@ -41,6 +59,13 @@ int main(){
     }
     int i=0;
     int j=n-1; //as array range 0 - (n-1)
+    printf("Unsorted Array: \n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
     mergesort(arr,i,j);
-
+    printf("\nSorted Array: \n");
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
+    }
 }
