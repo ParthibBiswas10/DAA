8669d2259de2cfdc20def7feef0d8bd938dbb522
ParthibBiswas10 <parthiblm10@gmail.com>
2025-06-18 10:07:33 +0530

    maxmin

--- maxmin_DivideConquer.c
+++ maxmin_DivideConquer.c
@@ -1,7 +1,14 @@
 #include<stdio.h>
+
+
 int main(){
     int n;
     printf("Enter Array Size: ");
     scanf("%d",&n);
-    
+    int arr[n];
+    printf("Enter Array: \n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    maxmin(arr,n);
 }
